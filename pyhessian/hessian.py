604259f326e97f14c8c0209e609f8357497d4859
THANNAGA <thom.hannagan@gmail.com>
2023-01-29 22:19:30 +0100

    Update hessian.py

--- pyhessian/hessian.py
+++ pyhessian/hessian.py
@@ -258,9 +258,9 @@ class hessian():
                 if i < len(alpha_list) - 1:
                     T[i + 1, i] = beta_list[i]
                     T[i, i + 1] = beta_list[i]
-            a_, b_ = torch.linalg.eig(T)
-            
+            a_, b_ = torch.linalg.eig(T)
             eigen_list = a_[:]
+            
             weight_list = b_[0, :]**2
             eigen_list_full.append(list(eigen_list.cpu().numpy()))
             weight_list_full.append(list(weight_list.cpu().numpy()))
